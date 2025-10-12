1709e0a5c94240494dd3040438d3bee2870c8347
NagyMateImre <nagym3@kkszi.hu>
2025-10-12 23:48:37 +0200

    valtozasok

--- js/Effect.js
+++ js/Effect.js
@@ -1,8 +1,15 @@
+import { totalCards } from "./Szuro";
+
 const Ingatlan = document.getElementById('Ingatlan');
 const Ugyfel = document.getElementById('Ugyfel');
 const Kezelt = document.getElementById('Kezelt');
-const osszes = docume.getElementById('osszes');
+const ertekek = document.getElementById('ertekek');
+const table = document.getElementById('table');
 
-document.addEventListener('DOMContentLoaded', (item) =>{
-    
+document.addEventListener('DOMContentLoaded', () =>{
+    Ingatlan.innerText = "5";
+    Ugyfel.innerText = "12";
+    Kezelt.innerText = totalCards;
 })
+
+
