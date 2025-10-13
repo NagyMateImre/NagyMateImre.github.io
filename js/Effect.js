ab92ba7ddf450e28e31db3ae205a991082331f5b
NagyMateImre <nagym3@kkszi.hu>
2025-10-13 23:51:08 +0200

    MindenIS

--- js/Effect.js
+++ js/Effect.js
@@ -1,15 +1,13 @@
-import { totalCards } from "./Szuro";
+let mysql = require('mysql2');
 
-const Ingatlan = document.getElementById('Ingatlan');
-const Ugyfel = document.getElementById('Ugyfel');
-const Kezelt = document.getElementById('Kezelt');
-const ertekek = document.getElementById('ertekek');
-const table = document.getElementById('table');
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Mate2007.0802"
+});
 
-document.addEventListener('DOMContentLoaded', () =>{
-    Ingatlan.innerText = "5";
-    Ugyfel.innerText = "12";
-    Kezelt.innerText = totalCards;
-})
-
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
